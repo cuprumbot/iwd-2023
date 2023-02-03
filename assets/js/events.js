cdbcce26f9e79d6ae7874d5de0829e9415d731b0
Yosef <yosefmarr@gmail.com>
2023-02-03 13:52:02 -0600

    2023

---
+++ assets/js/events.js
@@ -0,0 +1,12 @@
+var events = [
+    {
+        "name": "Ejemplificando",
+        "description": "Aprendiendo a preparar y explicar ejemplos.",
+        "speaker": "Alicia Ejemplo",
+        "day": "10",
+        "time": "09:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    }
+];
---
+++ assets/js/staff.js
@@ -0,0 +1,10 @@
+var staff = [
+    {
+        "name": "Alicia Ejemplo",
+        "short": "",
+        "information": "Alicia es entusiasta de la tecnología.\n\nLa biografía de Alicia tiene dos párrafos.",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    
+];
