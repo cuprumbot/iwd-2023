d1f1cd7a29bfb8757c88704cb936d5f51c5197a5
Luis Jimenez <luisrrjjcc@gmail.com>
2023-03-13 18:14:58 -0600

    staff

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,10 +1,20 @@
 var staff = [
-    {
+    /*{
         "name": "Alicia Ejemplo",
         "short": "",
         "information": "Alicia es entusiasta de la tecnología.\n\nLa biografía de Alicia tiene dos párrafos.",
         "image": "default.png",
         "nationality": "guatemala"
-    },
+    }*/
+
+    {
+        "name": "Nathaly Alarcón",
+        "short": "MSc en Ciencia de Datos, Google Developer Expert en Machine Learning",
+        "information": "MSc en Ciencia de Datos, Universidad de Granada - España\nGoogle Developer Expert in Machine Learning\
+Data Science Engineer Team Lead en Mojix\nDocente en cursos de Postgrado en materias de Data Science y Machine Learning\
+Women in Data Science Ambassador hasta el año 2020\nTomé cursos de Machine Learning en Stanford - USA y en Cifar - Canadá",
+        "image": "NathalyAlarcon.jpg",
+        "nationality": "bolivia"
+    }
     
 ];
