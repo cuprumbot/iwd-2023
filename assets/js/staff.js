85d1f78bc836c24783637df4042e1044b8322c28
Luis Cu <luiscu@galileo.edu>
2023-03-15 11:59:12 -0600

    solved merge conflict in staff.js

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,20 +1,5 @@
 var staff = [
-<<<<<<< HEAD
-/*
-    {
-        "name": "",
-=======
-    /*{
-        "name": "Alicia Ejemplo",
->>>>>>> d1f1cd7a29bfb8757c88704cb936d5f51c5197a5
-        "short": "",
-        "information": "",
-        "image": "default.png",
-        "nationality": "guatemala"
-<<<<<<< HEAD
-    },
-*/
-    
+
     /* COLOR VERDE EN LISTADO */ 
 
     {
