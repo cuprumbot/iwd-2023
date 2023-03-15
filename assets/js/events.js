15cc5cfd551161233769eb40414a9c14ff60524f
Luis Cu <luiscu@galileo.edu>
2023-03-15 11:56:48 -0600

    staff added

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,12 +1,483 @@
 var events = [
     {
-        "name": "Ejemplificando",
-        "description": "Aprendiendo a preparar y explicar ejemplos.",
-        "speaker": "Alicia Ejemplo",
-        "day": "10",
+        "name": "Women in Data Science",
+        "description": "",
+        "speaker": "Laura Gutierrez",
+        "day": "24",
         "time": "09:00",
         "place": "Track 1",
         "subjects": [],
         "tba": "0"
+    },
+    {
+        "name": "Brilla como eres: Descubre tu poder y alcanza tus sueños",
+        "description": "",
+        "speaker": "Mary Moreno",
+        "day": "24",
+        "time": "09:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Cómo ser recordado y tener un impacto en la vida de las personas",
+        "description": "",
+        "speaker": "Ilse Alejandra González Estrada",
+        "day": "24",
+        "time": "09:00",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Ciberseguridad, una oportunidad para todos",
+        "description": "",
+        "speaker": ["Lucila Barrios", "Verónica Klee"],
+        "day": "24",
+        "time": "09:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Involúcrate en STEM",
+        "description": "",
+        "speaker": "Diana Gutiérrez",
+        "day": "24",
+        "time": "09:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Cómo ser una verdadera líder",
+        "description": "",
+        "speaker": "Silvia Lucía Moya Morales",
+        "day": "24",
+        "time": "09:30",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Emprendimiento Femenino",
+        "description": "",
+        "speaker": "Andrea Cabrera",
+        "day": "24",
+        "time": "10:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "¿Cómo ser un estudiante virtual exitoso?",
+        "description": "",
+        "speaker": "Mariela Román Barrios",
+        "day": "24",
+        "time": "10:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "\"Yo Puedo Con Todo\"",
+        "description": "",
+        "speaker": "Marylin Lisset Martínez Dávila",
+        "day": "24",
+        "time": "10:00",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Innovando fuera de tu zona de confort",
+        "description": "",
+        "speaker": "Karine Odette Ávila Cofiño",
+        "day": "24",
+        "time": "10:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Construyendo mi futuro en cada paso",
+        "description": "",
+        "speaker": "Dalia Ester Azucena Quiroz de Leon",
+        "day": "24",
+        "time": "10:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Sos la única dueña de tu futuro",
+        "description": "",
+        "speaker": "Maria Alejandra Fuentes Chinchilla",
+        "day": "24",
+        "time": "10:30",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+
+    {
+        "name": "Break",
+        "description": "",
+        "speaker": "",
+        "day": "24",
+        "time": "11:00",
+        "place": "Todos los tracks",
+        "subjects": [],
+        "tba": "0"
+    },
+    
+    {
+        "name": "Del pupitre al buró: tips y anécdotas para desarrollarte en puestos de liderazgo",
+        "description": "",
+        "speaker": "María José Bejarano",
+        "day": "24",
+        "time": "11:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Ciencias del bosque: ingenieras e ingeniosas",
+        "description": "",
+        "speaker": "Dilia Díaz",
+        "day": "24",
+        "time": "11:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Mujeres creando con impresión 3D: ¡Innovación sin límites!",
+        "description": "",
+        "speaker": "Lissy Amador Sazo",
+        "day": "24",
+        "time": "11:30",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Emprendimiento social y Sheva",
+        "description": "",
+        "speaker": "Marisabel Ruiz",
+        "day": "24",
+        "time": "12:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Tomar decisiones para trascender",
+        "description": "",
+        "speaker": "Jenbli Julissa Miranda Flores de Fuentes",
+        "day": "24",
+        "time": "12:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "20 formas de sentirte empoderada",
+        "description": "",
+        "speaker": "Melissa Flores-Gómez Fiallos de Belches",
+        "day": "24",
+        "time": "12:00",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "No hay límites ni género para hacer arte con el cuero",
+        "description": "",
+        "speaker": "Sandra López",
+        "day": "24",
+        "time": "12:15",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Rompiendo los límites y paradigmas de una mujer jóven en una transnacional",
+        "description": "",
+        "speaker": "Yadira Graciela Nuyens Canté de Callejas",
+        "day": "24",
+        "time": "12:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Las mujeres en las finanzas e inteligencia artificial",
+        "description": "",
+        "speaker": "Marisol Catalán",
+        "day": "24",
+        "time": "12:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Aprender para emprender",
+        "description": "",
+        "speaker": "Ana Lucia Toledo",
+        "day": "24",
+        "time": "12:45",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Mujeres Líderes",
+        "description": "",
+        "speaker": "Katy Dayana Figueroa de Tock",
+        "day": "24",
+        "time": "12:45",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Biker chicks... ¿quién dijo miedo?",
+        "description": "",
+        "speaker": "Ana Gabriela Pantoja",
+        "day": "24",
+        "time": "12:45",
+        "place": "Track 3",
+        "subjects": [],
+        "tba": "0"
+    },
+
+    
+    /* SÁBADO */
+
+    {
+        "name": "AI",
+        "description": "",
+        "speaker": "Nathaly Alarcon",
+        "day": "25",
+        "time": "08:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Prototipado 101",
+        "description": "",
+        "speaker": "Sammy Arburola",
+        "day": "25",
+        "time": "08:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Liderazgo que impacta",
+        "description": "",
+        "speaker": "América Ixlem Chen Sam",
+        "day": "25",
+        "time": "09:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Liderazgo en nuevo siglo",
+        "description": "",
+        "speaker": "Karla Alejandra Miranda Vega",
+        "day": "25",
+        "time": "09:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Fintech: transformación digital en las finanzas",
+        "description": "",
+        "speaker": "Adriana Lizzeth Pinto Agustín",
+        "day": "25",
+        "time": "09:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "El Arte de Cobrar",
+        "description": "",
+        "speaker": "Guisela Margarita Estrada Guerra",
+        "day": "25",
+        "time": "09:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Liderazgo de las mujeres en pandemia COVID-19",
+        "description": "",
+        "speaker": "Nancy Virginia Sandoval Paiz",
+        "day": "25",
+        "time": "10:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "La mujer y su enemigo",
+        "description": "",
+        "speaker": "Gaby Gamas",
+        "day": "25",
+        "time": "10:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Construyendo un mundo más inclusivo una aplicación a la vez",
+        "description": "",
+        "speaker": "Cecilia Castillo",
+        "day": "25",
+        "time": "10:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Mujeres en Crecimiento dentro del BI",
+        "description": "",
+        "speaker": "Marcela Ambrosio",
+        "day": "25",
+        "time": "10:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+
+    {
+        "name": "Break",
+        "description": "",
+        "speaker": "",
+        "day": "25",
+        "time": "11:00",
+        "place": "Todos los tracks",
+        "subjects": [],
+        "tba": "0"
+    },
+
+    {
+        "name": "Encontrando tu poder",
+        "description": "",
+        "speaker": "Andrea Mazariegos",
+        "day": "25",
+        "time": "11:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Bailando Bajo la Tormenta Aprendí a ser Feliz",
+        "description": "",
+        "speaker": "Luisa Janet España Oliva",
+        "day": "25",
+        "time": "11:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Identidad Digital ¿Qué es y cómo puedes protegerte?",
+        "description": "",
+        "speaker": "Julia Marielos Argueta Revolorio",
+        "day": "25",
+        "time": "12:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Plenas y armónicas",
+        "description": "",
+        "speaker": ["Karen Pineda", "Karen Cerón"],
+        "day": "25",
+        "time": "12:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Web",
+        "description": "",
+        "speaker": "Mayra Rodríguez",
+        "day": "25",
+        "time": "12:30",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Intro a Design Systems 101",
+        "description": "",
+        "speaker": "Gaby Futch",
+        "day": "25",
+        "time": "12:30",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "La importancia de la transformación digital en las empresas",
+        "description": "",
+        "speaker": "Wendy Magaly Túnchez Pérez",
+        "day": "25",
+        "time": "13:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Cyberseguridad",
+        "description": "",
+        "speaker": "Meiling Ericelda Esteban García",
+        "day": "25",
+        "time": "13:00",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Atrévete a soñar",
+        "description": "",
+        "speaker": "Sandra Judith Castañeda García",
+        "day": "25",
+        "time": "13:15",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+    {
+        "name": "Las mujeres somos las más beneficiadas con la inteligencia artificial",
+        "description": "",
+        "speaker": "Janina Eunice Bonilla Smith",
+        "day": "25",
+        "time": "13:15",
+        "place": "Track 2",
+        "subjects": [],
+        "tba": "0"
     }
+
+/*
+    {
+        "name": "",
+        "description": "",
+        "speaker": "",
+        "day": "25",
+        "time": "09:00",
+        "place": "Track 1",
+        "subjects": [],
+        "tba": "0"
+    },
+*/
 ];
--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,10 +1,339 @@
 var staff = [
+/*
     {
-        "name": "Alicia Ejemplo",
+        "name": "",
         "short": "",
-        "information": "Alicia es entusiasta de la tecnología.\n\nLa biografía de Alicia tiene dos párrafos.",
+        "information": "",
         "image": "default.png",
         "nationality": "guatemala"
     },
+*/
+    
+    /* COLOR VERDE EN LISTADO */ 
+
+    {
+        "name": "Ilse Alejandra González Estrada",
+        "short": "",
+        "information": "",
+        "image": "IlseGonzalez.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Andrea Cabrera",
+        "short": "",
+        "information": "",
+        "image": "AndreaCabrera.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Mariela Román Barrios",
+        "short": "",
+        "information": "",
+        "image": "MarielaRoman.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Marylin Lisset Martínez Dávila",
+        "short": "",
+        "information": "",
+        "image": "MarylinMartinez.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Karine Odette Ávila Cofiño",
+        "short": "",
+        "information": "",
+        "image": "KarineAvila.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "María Alejandra Fuentes Chinchilla",
+        "short": "",
+        "information": "",
+        "image": "MariaFuentes.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Lissy Amador Sazo",
+        "short": "",
+        "information": "",
+        "image": "LissyAmador.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Marisabel Ruiz",
+        "short": "",
+        "information": "",
+        "image": "MarisabelRuiz.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Ana Lucía Toledo",
+        "short": "",
+        "information": "",
+        "image": "AnaToledo.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Adriana Lizzeth Pinto Agustín",
+        "short": "",
+        "information": "",
+        "image": "AdrianaPinto.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Guisela Margarita Estrada Guerra",
+        "short": "",
+        "information": "",
+        "image": "GuiselaEstrada.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Nancy Virginia Sandoval Paiz",
+        "short": "",
+        "information": "",
+        "image": "NancySandoval.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Marcela Ambrosio",
+        "short": "",
+        "information": "",
+        "image": "MarcelaAmbrosio.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Andrea Mazariegos",
+        "short": "",
+        "information": "",
+        "image": "AndreaMazariegos.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Julia Marielos Argueta Revolorio",
+        "short": "",
+        "information": "",
+        "image": "JuliaArgueta.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Sammy Arburola",
+        "short": "",
+        "information": "",
+        "image": "SammyArburola.jpg",
+        "nationality": "guatemala"
+    },
+
+    /* COLOR VERDE PERO NO MANDARON CFP */
+    
+    {
+        "name": "Laura Gutierrez",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Mary Moreno",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Maria José Bejarano",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Dilia Diaz",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Sandra Lopez",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Gaby Gamas",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Cecilia Castillo",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Lucila Barrios",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Verónica Klee",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+
+    /* OTROS COLORES */
+
+    {
+        "name": "Diana Gutierrez",
+        "short": "",
+        "information": "",
+        "image": "DianaGutierrez.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Silvia Lucía Moya Morales",
+        "short": "",
+        "information": "",
+        "image": "SilviaMoya.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Dalia Ester Azucena Quiroz de Leon",
+        "short": "",
+        "information": "",
+        "image": "DaliaQuiroz.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Jenbli Julissa Miranda Flores de Fuentes",
+        "short": "",
+        "information": "",
+        "image": "JenbliMiranda.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Melissa Flores-Gómez Fiallos de Belches",
+        "short": "",
+        "information": "",
+        "image": "MelissaFlores.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Yadira Graciela Nuyens Canté de Callejas",
+        "short": "",
+        "information": "",
+        "image": "YadiraNuyens.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Marisol Catalán",
+        "short": "",
+        "information": "",
+        "image": "MarisolCatalan.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Katy Dayana Figueroa de Tock",
+        "short": "",
+        "information": "",
+        "image": "KatyFigueroa.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Ana Gabriela Pantoja",
+        "short": "",
+        "information": "",
+        "image": "AnaPantoja.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "América Ixlem Chen Sam",
+        "short": "",
+        "information": "",
+        "image": "AmericaChen.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Karla Alejandra Miranda Vega",
+        "short": "",
+        "information": "",
+        "image": "default.png",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Luisa Janet España Oliva",
+        "short": "",
+        "information": "",
+        "image": "LuisaEspana.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Karen Pineda",
+        "short": "",
+        "information": "",
+        "image": "KarenKaren.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Karen Cerón",
+        "short": "",
+        "information": "",
+        "image": "KarenKaren.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Gaby Futch",
+        "short": "",
+        "information": "",
+        "image": "GabyFutch.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Wendy Magaly Túnchez Pérez",
+        "short": "",
+        "information": "",
+        "image": "WendyTunchez.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Meiling Ericelda Esteban García",
+        "short": "",
+        "information": "",
+        "image": "MeilingEsteban.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Sandra Judith Castañeda García",
+        "short": "",
+        "information": "",
+        "image": "SandraCastaneda.jpg",
+        "nationality": "guatemala"
+    },
+    {
+        "name": "Janina Eunice Bonilla Smith",
+        "short": "",
+        "information": "",
+        "image": "JaninaBonilla.jpg",
+        "nationality": "guatemala"
+    },
+
+    /* OTROS */
+
+    {
+        "name": "Nathaly Alarcon",
+        "short": "",
+        "information": "MSc en Ciencia de Datos. Universidad de Granada - España.\n\nGoogle Developer Expert in Machine Learning\n\nData Science Engineer Team Lead en Mojix\n\nDocente en cursos de Postgrado en materias de Data Science y Machine Learning.\n\nWomen in Data Science Ambassador hasta el año 2020\n\nTomé cursos de Machine Learning en Stanford - USA y en Cifar - Canadá",
+        "image": "NathalyAlarcon.jpg",
+        "nationality": "bolivia"
+    }
     
 ];
